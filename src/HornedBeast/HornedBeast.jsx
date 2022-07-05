1d9f1d5fd9cd1ba90844d5cb2cb24d5fe5ec8f5c
C Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-07-05 17:07:22 +0100

    initial commit

---
+++ src/HornedBeast/HornedBeast.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class HornedBeast extends Component{
+    render() {
+        return (
+            <div>
+                <h1>HornedBeast</h1>
+            </div>
+        )
+    }
+}
+
+export default HornedBeast;
---
+++ src/Main/Main.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import HornedBeast from "../HornedBeast/HornedBeast";
+
+class Main extends Component {
+	render() {
+		return (
+            <div className="main">
+                <HornedBeast />
+                <HornedBeast />
+
+                
+            </div>
+        )
+	}
+}
+
+export default Main;
