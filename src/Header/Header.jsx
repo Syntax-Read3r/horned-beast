1554347a7a79c3fc5d1b18dde22be0ce9a29370b
Wollivan <tim.smith2814@gmail.com>
2022-07-07 10:04:44 +0100

    Stretch goals together

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,30 @@
 import { Component } from "react";
+import "./Header.css";
 
 class Header extends Component {
-    render() {
-        return (
-            <header>
-                <h1>Header</h1>
-            </header>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      colorSwitch: true,
+    };
+  }
+
+  changeHeaderColor = () => {
+    //do something
+    this.setState({ colorSwitch: !this.state.colorSwitch });
+  };
+
+  render() {
+    return (
+      <header onClick={this.changeHeaderColor}>
+        {this.state.colorSwitch ? (
+          <h1 className="purpleHeader">Header</h1>
+        ) : (
+          <h1 className="blueHeader">Header</h1>
+        )}
+      </header>
+    );
+  }
 }
 
 export default Header;
